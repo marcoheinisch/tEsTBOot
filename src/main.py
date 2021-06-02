85b13ad3e8099973605f662156d2aa40b32db0eb
marcoheinisch <57726217+marcoheinisch@users.noreply.github.com>
2021-06-02 13:10:07 +0200

    add bot status

--- src/main.py
+++ src/main.py
@@ -9,7 +9,8 @@ import os
 import random
 import asyncio
 
-import discord 
+import discord
+from discord import activity 
 from discord.ext import commands
 from dotenv import load_dotenv
 import requests
@@ -17,15 +18,16 @@ import requests
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-
-bot = commands.Bot(command_prefix='!')
-
+MESSAGE_CHANNEL = "admin"
 TXT_VOICE_UPDATE = ["is needy and wait's for academic trash talk", 
-                    "is lonely and want's to talk", 
+                    #"is lonely and want's to talk", 
                     "is waiting for you ",
                     "is sitting alone here",
                     "<put here some random text stuff>"
                     ]
+
+activity = discord.Game(name="mit Feuer!🔥🔥🔥")#discord.Activity(type=discord.ActivityType.watching, name="you!")
+bot = commands.Bot(command_prefix='!', activity=activity)#, status=discord.Status.dnd)
 
 # Events
 
@@ -36,11 +38,14 @@ async def on_voice_state_update(member, before, after):
         print("lonely state")
         await asyncio.sleep(10) # wait to est if user is shy / has misclicked
         if after.channel is not None:
+            print("trigger")
             guild = discord.utils.get(bot.guilds, name=GUILD)
             voice_channel = discord.utils.get(guild.voice_channels, name=channel_name)
+            print(voice_channel.voice_states)
             if len(voice_channel.voice_states)==1:
-                text_channel = discord.utils.get(guild.text_channels, name="📯mitteilungen")
-                await text_channel.send(f"Moin moin! {member.name} "+random.choice(TXT_VOICE_UPDATE)+". Visit him at #"+after.channel.name+".")
+                print("t2")
+                text_channel = discord.utils.get(guild.text_channels, name=MESSAGE_CHANNEL)
+                await text_channel.send(f"Moin! {member.name} "+random.choice(TXT_VOICE_UPDATE)+". Visit him at #"+after.channel.name+".")
 
 @bot.event
 async def on_ready():
@@ -88,12 +93,12 @@ async def roll(ctx, emoji_name: str, image_url:str):
     response = requests.get(image_url)
     img = response.content   
     img = await ctx.guild.create_custom_emoji(name=emoji_name, image=img)
-    await ctx.send(">> Emoji erstellt: "+str(img))
+    await ctx.send(">> Emoji created: "+str(img))
 
 @bot.event
 async def on_command_error(ctx, error):
     print(error.__cause__)
-    await ctx.send(">> Fehler:"+str(error.__cause__))
+    await ctx.send(">> Error:"+str(error.__cause__))
     
 
 bot.run(TOKEN)
