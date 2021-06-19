89c1e5e85e4c42b49c9542c9df7c0591d9d358f6
marcoheinisch <57726217+marcoheinisch@users.noreply.github.com>
2021-06-19 22:55:41 +0200

    reorganize code

---
+++ commands.py
@@ -0,0 +1,128 @@
+from discord.ext import commands
+
+import random
+import requests
+import wolframalpha
+
+class Commands(commands.Cog):
+    """A couple of commands."""
+
+    def __init__(self, bot: commands.Bot, wolframclient):
+        self.bot = bot
+        self.wolframclient = wolframclient
+
+
+    @commands.command(name='roll_dice')
+    async def roll(self, ctx: commands.Context, number_of_dice: int, number_of_sides: int):
+        """Simulates rolling dice."""
+        print("roll event!")
+
+        dice = [
+            str(random.choice(range(1, number_of_sides + 1)))
+            for _ in range(number_of_dice)
+        ]
+        print(dice)
+        await ctx.send(', '.join(dice))
+
+
+    @commands.command(name='hi')
+    async def roll(self, ctx: commands.Context, number_of_hi: int = 1):
+        """Say \"Hi!\" multiple times."""
+        print("hi!")
+
+        for _ in range(number_of_hi):
+            await ctx.send('Hi!')
+
+
+    @commands.command(name='echo', help='Echo string.')
+    async def roll(self, ctx: commands.Context, *, txt:str):
+        """Echos input string."""
+        print("echo!")
+
+        await ctx.send(txt)
+
+
+    @commands.command(name='emoji')
+    async def roll(self, ctx: commands.Context, emoji_name: str, image_url:str):
+        """Creates custom server emoji. 
+        Supports .jpg .gif .png."""
+        print("emoji!")
+
+        response = requests.get(image_url)
+        img = response.content   
+        img = await ctx.guild.create_custom_emoji(name=emoji_name, image=img)
+        await ctx.send(">> Emoji created: "+str(img))
+
+
+    @commands.command(name='molec')
+    async def roll(self, ctx: commands.Context, smile_string: str):
+        """'Visualize a given molecule string. Supports MIME and other structural identifier. 
+        Note: Triple bonds in SMILES strings represented by \'\#\' have to be URL-escaped as \'%23\' and \'?\' as \'%3F\'."""
+        print('molec!')
+
+        url1 = 'http://cactus.nci.nih.gov/chemical/structure/' + smile_string+ '/image'
+        await ctx.send(">> Molecule: "+ str(url1))
+        
+
+    @commands.command(name='wolfram')
+    async def roll(self, ctx: commands.Context, *, question_string: str):
+        """Use Wolfram Alpha (API) to solve Math or ask random stuff It can do ...
+        everything WolframAlpha can do: Equations, Weather  (Overview: https://www.wolframalpha.com/)"""
+        print('wolfram! ' + question_string)
+
+        res = self.wolframclient .query(question_string)
+        if not res.success:
+            await ctx.send(">> Wolfram Weisnisch Weiter... ")
+            return
+        try:
+            message = next(res.results).text
+        except StopIteration:
+            message = "No short result found. Try \"!wolfram-l\"."
+        await ctx.send(">> Wolfram: "+ message)
+
+
+    @commands.command(name='wolfall')
+    async def roll(self, ctx: commands.Context, *, question_string: str):
+        """See !wolfram. Returns long informative answer"""
+        print('wolfram! ' + question_string)
+
+        res = self.wolframclient .query(question_string)
+        if not res.success:
+            await ctx.send(">> Wolfram Weisnisch Weiter... ")
+            return
+
+        message = ""    
+        for pod in res.pods:
+            if pod.title == res.datatypes:
+                message += str(pod.subpod.img.src) + "\n"
+            for sub in pod.subpods:
+                if sub.plaintext:
+                    message += str(sub.plaintext) + "\n"
+        await ctx.send(">> Wolfram: "+ message)
+
+
+    @commands.command(name='wolfget')
+    async def roll(self, ctx: commands.Context, image_title: str, question_string: str):
+        """See !wolfram. Returns image with given title"""
+        print('wolfram! ' + question_string)
+
+        res = self.wolframclient .query(question_string)
+        if not res.success:
+            await ctx.send(">> Wolfram Weisnisch Weiter... ")
+            return
+
+        message = ""    
+        try:
+            message = next(res.results).text+"\n"
+        except StopIteration:
+            pass
+        for pod in res.pods:
+            if pod.title == image_title:
+                message += str(pod.subpod.img.src) + "\n"
+        await ctx.send(">> Wolfram: "+ message)
+
+
+    @commands.event
+    async def on_command_error(self, ctx: commands.Context, error):
+        print(error.__cause__)
+        await ctx.send(">> Error: "+str(error.__cause__))
--- src/main.py
+++ src/main.py
@@ -11,22 +11,16 @@
 import os
 import random
 import asyncio
-from socket import socket
 
 import discord
-from discord import activity
-from discord.embeds import Embed 
 from discord.ext import commands
 from discord.ext import tasks
-from dns.rcode import NOERROR
-
-import requests
-from nested_lookup import nested_lookup
 
 from mcstatus import MinecraftServer
 import wolframalpha
 
-# DEBUG:
+from commands import Commands
+
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
@@ -46,13 +40,13 @@ TXT_VOICE_UPDATE = ["is needy and wait's for academic trash talk",
 
 basic_activity_name =" in der Cloud! ☁"
 bot = commands.Bot(command_prefix="!", activity= discord.Game(name=basic_activity_name))
-
 wolframclient = wolframalpha.Client(WOLFRAM_APPID)
 
 # Initialization errors
 
 if not (TOKEN and GUILD and WOLFRAM_APPID):
     raise RuntimeError("Missing environmental variable.")
+
 
 # Tasks
 
@@ -77,6 +71,7 @@ async def check_mc_status():
         mc_status = " mit "+("einem Spieler" if (players==1) else str(players)+" Spielern")+" MC!"
 
     await bot.change_presence(activity = discord.Game(name=mc_status))
+
 
 # Events
 
@@ -112,104 +107,10 @@ async def on_member_join(member):
 async def on_message(message):
     if message.author == bot.user:
         return
-    #read messageses   
+    # here read messageses   
     await bot.process_commands(message)
 
-# Commands
-
-@bot.command(name='roll_dice', help='Simulates rolling dice.')
-async def roll(ctx, number_of_dice: int, number_of_sides: int):
-    print("roll event!")
-    dice = [
-        str(random.choice(range(1, number_of_sides + 1)))
-        for _ in range(number_of_dice)
-    ]
-    print(dice)
-    await ctx.send(', '.join(dice))
-
-@bot.command(name='hi', help='Say \"Hi!\" multiple times.')
-async def roll(ctx, number_of_hi: int = 1):
-    print("hi!")
-    for _ in range(number_of_hi):
-        await ctx.send('Hi!')
-
-@bot.command(name='echo', help='Echo string.')
-async def roll(ctx, *, txt:str):
-    print("echo!")
-    await ctx.send(txt)
-
-@bot.command(name='emoji', help='Creates custom server emoji.')
-async def roll(ctx, emoji_name: str, image_url:str):
-    print("emoji!")
-    response = requests.get(image_url)
-    img = response.content   
-    img = await ctx.guild.create_custom_emoji(name=emoji_name, image=img)
-    await ctx.send(">> Emoji created: "+str(img))
-
-@bot.command(name='molec', help='Visualize a given molecule string. Supports MIME and other structural identifier. Note: Triple bonds in SMILES strings represented by \'\#\' have to be URL-escaped as \'%23\' and \'?\' as \'%3F\'. ', brief='Visualize a given molecule string.')
-async def roll(ctx, smile_string: str):
-    print('molec!')
-    url1 = 'http://cactus.nci.nih.gov/chemical/structure/' + smile_string+ '/image'
-    await ctx.send(">> Molecule: "+ str(url1))
-    
-@bot.command(name='wolfram')
-async def roll(ctx, *, question_string: str):
-    """Use Wolfram Alpha (API) to solve Math or ask random stuff It can do ...
-    everything WolframAlpha can do: Equations, Weather  (Overview: https://www.wolframalpha.com/)"""
-    print('wolfram! ' + question_string)
-
-    res = wolframclient.query(question_string)
-    if not res.success:
-        await ctx.send(">> Wolfram Weisnisch Weiter... ")
-        return
-    try:
-        message = next(res.results).text
-    except StopIteration:
-        message = "No short result found. Try \"!wolfram-l\"."
-    await ctx.send(">> Wolfram: "+ message)
-
-@bot.command(name='wolfram-l')
-async def roll(ctx, *, question_string: str):
-    """Use Wolfram Alpha (API) and return long answer.  (Overview: https://www.wolframalpha.com/)"""
-    print('wolfram! ' + question_string)
-
-    res = wolframclient.query(question_string)
-    if not res.success:
-        await ctx.send(">> Wolfram Weisnisch Weiter... ")
-        return
-
-    message = ""    
-    for pod in res.pods:
-        if pod.title == res.datatypes:
-            message += str(pod.subpod.img.src) + "\n"
-        for sub in pod.subpods:
-            if sub.plaintext:
-                message += str(sub.plaintext) + "\n"
-    await ctx.send(">> Wolfram: "+ message)
-
-@bot.command(name='wolfram-get')
-async def roll(ctx, image_title: str, question_string: str):
-    """Get img.  (Overview: https://www.wolframalpha.com/)"""
-    print('wolfram! ' + question_string)
-
-    res = wolframclient.query(question_string)
-    if not res.success:
-        await ctx.send(">> Wolfram Weisnisch Weiter... ")
-        return
-
-    message = ""    
-    for pod in res.pods:
-        if pod.title == image_title:
-            message += str(pod.subpod.img.src) + "\n"
-        for sub in pod.subpods:
-            if sub.plaintext:
-                message += str(sub.plaintext) + "\n"
-    await ctx.send(">> Wolfram: "+ message)
-
-@bot.event
-async def on_command_error(ctx, error):
-    print(error.__cause__)
-    await ctx.send(">> Error: "+str(error.__cause__))
+bot.add_cog(Commands(bot, wolframclient))
 
 bot.run(TOKEN)
 
