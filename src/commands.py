fb50f10bbe9ea5f7a47f05d6bc1061995b5a8bf4
marcoheinisch <57726217+marcoheinisch@users.noreply.github.com>
2021-09-02 21:49:25 +0200

    Reformat and add channeldisplay

--- src/commands.py
+++ src/commands.py
@@ -1,44 +1,52 @@
-from discord.ext import commands
+import boto3
+import os
+import random
+import requests
+
+import wolframalpha
 from discord import Embed
+from discord.ext import commands
+from discord.ext import tasks
+from mcstatus import MinecraftServer
 
-import random, requests, wolframalpha, os, boto3
-
-COLOR = [0xFFE4E1, 0x00FF7F, 0xD8BFD8, 0xDC143C, 0xFF4500, 0xDEB887, 0xADFF2F, 0x800000, 0x4682B4, 0x006400, 0x808080, 0xA0522D, 0xF08080, 0xC71585, 0xFFB6C1, 0x00CED1]
+COLOR = [0xFFE4E1, 0x00FF7F, 0xD8BFD8, 0xDC143C, 0xFF4500, 0xDEB887, 0xADFF2F, 0x800000, 0x4682B4, 0x006400, 0x808080,
+         0xA0522D, 0xF08080, 0xC71585, 0xFFB6C1, 0x00CED1]
+LOADING_EMOJI = ["➕", "➗", "➖", "➗"]
 
 AWS_SERVER_PUBLIC_KEY = os.getenv('AWS_SERVER_PUBLIC_KEY')
 AWS_SERVER_SECRET_KEY = os.getenv('AWS_SERVER_SECRET_KEY')
 WOLFRAM_APPID = os.getenv('WOLFRAM_APPID')
 
-if not (WOLFRAM_APPID and AWS_SERVER_SECRET_KEY and AWS_SERVER_PUBLIC_KEY):
-    raise RuntimeError("Missing environmental variable (2).")
+conf = {
+    "timeout_random": 60,
+    "aws_mc_checktime": 1,
+    "aws_mc_server_adress": "ratius99.aternos.me"
+}
+
 
 class MainCommands(commands.Cog):
     """A couple of commands."""
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.config = {
-            "timeout_random": 60
-        }
-
 
     @commands.command(name='random')
     async def random(self, ctx: commands.Context, number_of_dice: int, number_of_sides: int):
         """Simulates rolling dice."""
         print("random event!")
 
-        embed = Embed(title="Zufallszahlen",color=random.choice(COLOR), description=f"{number_of_dice} Würfel mit {number_of_sides} Seiten:")
+        embed = Embed(title="Zufallszahlen", color=random.choice(COLOR),
+                      description=f"{number_of_dice} Würfel mit {number_of_sides} Seiten:")
 
         for _ in range(number_of_dice):
-            w =  str(random.choice(range(1, number_of_sides + 1)))
-            embed.add_field(name=w, value = "_"*len(w)+"/"+str(number_of_sides), inline=True)
+            w = str(random.choice(range(1, number_of_sides + 1)))
+            embed.add_field(name=w, value="_" * len(w) + "/" + str(number_of_sides), inline=True)
 
-        timeout = self.config["timeout_random"]        
+        timeout = conf["timeout_random"]
         embed.set_footer(text=f"Selbstlöschend nach {timeout} Sekunden.")
 
         await ctx.message.delete()
         await ctx.send(embed=embed, delete_after=timeout)
-
 
     @commands.command(name='hi')
     async def hi(self, ctx: commands.Context, number_of_hi: int = 1):
@@ -48,26 +56,23 @@ class MainCommands(commands.Cog):
         for _ in range(number_of_hi):
             await ctx.send('Hi!')
 
-
     @commands.command(name='echo', help='Echo string.')
-    async def echo(self, ctx: commands.Context, *, txt:str):
+    async def echo(self, ctx: commands.Context, *, txt: str):
         """Echos input string."""
         print("echo!")
 
         await ctx.send(txt)
 
-
     @commands.command(name='emoji')
-    async def emoji(self, ctx: commands.Context, emoji_name: str, image_url:str):
+    async def emoji(self, ctx: commands.Context, emoji_name: str, image_url: str):
         """Creates custom server emoji. 
         Supports .jpg .gif .png."""
         print("emoji!")
 
         response = requests.get(image_url)
-        img = response.content   
+        img = response.content
         img = await ctx.guild.create_custom_emoji(name=emoji_name, image=img)
-        await ctx.send(">> Emoji created: "+str(img))
-
+        await ctx.send(">> Emoji created: " + str(img))
 
     @commands.command(name='molec')
     async def molec(self, ctx: commands.Context, smile_string: str):
@@ -75,9 +80,8 @@ class MainCommands(commands.Cog):
         Note: Triple bonds in SMILES strings represented by \'\#\' have to be URL-escaped as \'%23\' and \'?\' as \'%3F\'."""
         print('molec!')
 
-        url1 = 'http://cactus.nci.nih.gov/chemical/structure/' + smile_string+ '/image'
-        await ctx.send(">> Molecule: "+ str(url1))
-        
+        url1 = 'http://cactus.nci.nih.gov/chemical/structure/' + smile_string + '/image'
+        await ctx.send(">> Molecule: " + str(url1))
 
 
 class WolframCommands(commands.Cog):
@@ -86,8 +90,6 @@ class WolframCommands(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.wolframclient = wolframalpha.Client(WOLFRAM_APPID)
-        self.config = {}
-
 
     @commands.command(name='wolfram')
     async def wolfram(self, ctx: commands.Context, *, question_string: str):
@@ -95,7 +97,7 @@ class WolframCommands(commands.Cog):
         everything WolframAlpha can do: Equations, Weather  (Overview: https://www.wolframalpha.com/)"""
         print('wolfram! ' + question_string)
 
-        res = self.wolframclient .query(question_string)
+        res = self.wolframclient.query(question_string)
         if not res.success:
             await ctx.send(">> Wolfram Weisnisch Weiter... ")
             return
@@ -103,48 +105,46 @@ class WolframCommands(commands.Cog):
             message = next(res.results).text
         except StopIteration:
             message = "No short result found. Try \"!wolfram-l\"."
-        await ctx.send(">> Wolfram: "+ message)
-
+        await ctx.send(">> Wolfram: " + message)
 
     @commands.command(name='wolfall')
     async def wolfall(self, ctx: commands.Context, *, question_string: str):
         """See !wolfram. Returns long informative answer"""
         print('wolfram! ' + question_string)
 
-        res = self.wolframclient .query(question_string)
+        res = self.wolframclient.query(question_string)
         if not res.success:
             await ctx.send(">> Wolfram Weisnisch Weiter... ")
             return
 
-        message = ""    
+        message = ""
         for pod in res.pods:
             if pod.title == res.datatypes:
                 message += str(pod.subpod.img.src) + "\n"
             for sub in pod.subpods:
                 if sub.plaintext:
                     message += str(sub.plaintext) + "\n"
-        await ctx.send(">> Wolfram: "+ message)
-
+        await ctx.send(">> Wolfram: " + message)
 
     @commands.command(name='wolfget')
     async def wolfget(self, ctx: commands.Context, image_title: str, question_string: str):
         """See !wolfram. Returns image with given title"""
         print('wolfram! ' + question_string)
 
-        res = self.wolframclient .query(question_string)
+        res = self.wolframclient.query(question_string)
         if not res.success:
             await ctx.send(">> Wolfram Weisnisch Weiter... ")
             return
 
-        message = ""    
+        message = ""
         try:
-            message = next(res.results).text+"\n"
+            message = next(res.results).text + "\n"
         except StopIteration:
             pass
         for pod in res.pods:
             if pod.title == image_title:
                 message += str(pod.subpod.img.src) + "\n"
-        await ctx.send(">> Wolfram: "+ message)
+        await ctx.send(">> Wolfram: " + message)
 
 
 class AWSCommands(commands.Cog):
@@ -152,9 +152,37 @@ class AWSCommands(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-       
-        self.config = {}
+        self.startuploop.start()
+        self.aws_status = 0
+        self.aws_loading_count = 0
 
+    # Tasks
+
+    @commands.tasks.loop(minutes=1)
+    async def startuploop(self, bot):
+        print("loopaws")
+
+        self.aws_loading_count += 1
+
+        try:
+            server = MinecraftServer.lookup("3.125.141.61")
+            status = server.status()
+            players = status.players.online
+            self.aws_status = 1
+
+        except Exception:
+            if self.aws_status == 1:
+                self.aws_status = 0
+
+        channel = bot.get_channel(852114543759982592)
+        if self.aws_status == 0:
+            await channel.edit(name='❌OFFLINE')
+        if self.aws_status == 1:
+            await channel.edit(name=f"✔ONLINE ({str(players) if players else 0} Spieler)")
+        if self.aws_status == 2:
+            await channel.edit(name=f'{LOADING_EMOJI[self.aws_loading_count % len(LOADING_EMOJI)]}WAITING')
+        if self.aws_status == 22:
+            await channel.edit(name=f'{LOADING_EMOJI[self.aws_loading_count % len(LOADING_EMOJI)]}STARTING')
 
     @commands.command(name='aws')
     async def aws(self, ctx: commands.Context, command: str):
@@ -169,7 +197,7 @@ class AWSCommands(commands.Cog):
 
         ec2 = session.resource('ec2')
         self.instance = ec2.Instance('i-07baa970d1c82bb08')
-        
+
         def turnOffInstance():
             try:
                 self.instance.stop()
@@ -181,6 +209,7 @@ class AWSCommands(commands.Cog):
         def turnOnInstance():
             try:
                 self.instance.start()
+                self.startuploop.start()
                 return True
             except Exception as e:
                 print(e)
@@ -199,7 +228,7 @@ class AWSCommands(commands.Cog):
 
         if 'stop' in command:
             if turnOffInstance():
-                await  ctx.send('AWS Instance stopping')
+                await ctx.send('AWS Instance stopping')
             else:
                 await ctx.send('Error stopping AWS Instance')
         elif 'start' in command:
@@ -214,4 +243,3 @@ class AWSCommands(commands.Cog):
                 await ctx.send('AWS Instance rebooting')
             else:
                 await ctx.send('Error rebooting AWS Instance')
-
--- src/main.py
+++ src/main.py
@@ -17,20 +17,18 @@ from discord.ext import commands
 from discord.ext import tasks
 
 from mcstatus import MinecraftServer
-
 from src.commands import MainCommands, WolframCommands, AWSCommands
-
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-MC_SERVER_CHECK_TIME = 10 #minutes
+MC_SERVER_CHECK_TIME = 10  # minutes
 MC_SERVER_ADDRESS = "ratius99.aternos.me"
 MC_SERVER_STATUS_INT = 0
 
 MESSAGE_CHANNEL = "📯mitteilungen"
-TXT_VOICE_UPDATE = ["is needy and wait's for academic trash talk", 
-                    "is lonely and want's to talk", 
+TXT_VOICE_UPDATE = ["is needy and wait's for academic trash talk",
+                    "is lonely and want's to talk",
                     "is waiting for you ",
                     "is sitting alone here",
                     "wants to procrastinate",
@@ -40,15 +38,8 @@ TXT_VOICE_UPDATE = ["is needy and wait's for academic trash talk",
                     "is hiding a bomb bellow his desk."
                     ]
 
-basic_activity_name =" in der Cloud! ☁"
-bot = commands.Bot(command_prefix="!", activity= discord.Game(name=basic_activity_name))
-
-
-# Initialization errors
-
-if not (TOKEN and GUILD):
-    raise RuntimeError("Missing environmental variable.")
-
+basic_activity_name = " in der Cloud! ☁"
+bot = commands.Bot(command_prefix="!", activity=discord.Game(name=basic_activity_name))
 
 # Tasks
 
@@ -58,7 +49,7 @@ async def check_mc_status():
 
     mc_status = basic_activity_name
     players = 0
-    
+
     try:
         server = MinecraftServer.lookup(MC_SERVER_ADDRESS)
         status = server.status()
@@ -69,34 +60,37 @@ async def check_mc_status():
         mc_status = " mit \"bad status error\" :-("
 
     # if no error happend:
-    if (players):
-        mc_status = " mit "+("einem Spieler" if (players==1) else str(players)+" Spielern")+" MC!"
+    if players:
+        mc_status = " mit " + ("einem Spieler" if (players == 1) else str(players) + " Spielern") + " MC!"
 
-    await bot.change_presence(activity = discord.Game(name=mc_status))
+    await bot.change_presence(activity=discord.Game(name=mc_status))
 
 
 # Events
 
-@bot.event 
+@bot.event
 async def on_voice_state_update(member, before, after):
     if before.channel is None and after.channel is not None:
-        channel_name=after.channel.name
+        channel_name = after.channel.name
         print("lonely state")
-        await asyncio.sleep(10) # wait to est if user is shy / has misclicked
+        await asyncio.sleep(10)  # wait to est if user is shy / has misclicked
         if after.channel is not None:
             print("trigger")
             guild = discord.utils.get(bot.guilds, name=GUILD)
             voice_channel = discord.utils.get(guild.voice_channels, name=channel_name)
             print(voice_channel.voice_states)
-            if len(voice_channel.voice_states)==1:
+            if len(voice_channel.voice_states) == 1:
                 print("t2")
                 text_channel = discord.utils.get(guild.text_channels, name=MESSAGE_CHANNEL)
-                await text_channel.send(f"Moin! {member.name} "+random.choice(TXT_VOICE_UPDATE)+". Visit him at #"+after.channel.name+".")
+                await text_channel.send(f"Moin! {member.name} " + random.choice(
+                    TXT_VOICE_UPDATE) + ". Visit him at #" + after.channel.name + ".")
+
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
     check_mc_status.start()
+
 
 @bot.event
 async def on_member_join(member):
@@ -105,24 +99,26 @@ async def on_member_join(member):
         f'Hi {member.name}, hier issst der nerfffiger Diiscordbot aus Konziis!'
     )
 
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
-    # here read messageses   
+    # here read messages
     await bot.process_commands(message)
+
 
 @bot.event
 async def on_command_error(ctx: commands.Context, error):
     print(error.__cause__)
-    await ctx.send(">> Error: "+str(error.__cause__))
+    await ctx.send(">> Error: " + str(error.__cause__))
+
 
 bot.add_cog(MainCommands(bot))
 bot.add_cog(WolframCommands(bot))
 bot.add_cog(AWSCommands(bot))
 
 bot.run(TOKEN)
-
 
 # Custom event example:
 # 
